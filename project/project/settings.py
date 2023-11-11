4a7f711d06774a0b404c931b13112a84d2f5e838
sakiadachi <sakiadachi.88@gmail.com>
2023-11-11 21:06:24 +0900

    Read ALLOWED_HOSTS from environ

--- project/project/settings.py
+++ project/project/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEBUG" in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(",")
 
 
 # Application definition
