4410eef8a81611102c68ee884ec2ac11073836fc
sakiadachi <sakiadachi.88@gmail.com>
2023-11-11 20:57:56 +0900

    Load SECRET_KEY from environ

--- project/project/settings.py
+++ project/project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 import dj_database_url
 from dotenv import (
@@ -26,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*vw+j+@^7+nq+@!yibx2%5u8s26i3cg13$v2(pq$l(!!vwc@2o'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
