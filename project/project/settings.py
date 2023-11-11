64323710942352af9b0f9e1e52ab3729c02160ba
sakiadachi <sakiadachi.88@gmail.com>
2023-11-11 20:56:56 +0900

    Load env vars from .env

--- project/project/settings.py
+++ project/project/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 import dj_database_url
+from dotenv import (
+    load_dotenv,
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,6 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-*vw+j+@^7+nq+@!yibx2%5u8s26i3cg13$v2(pq$l(!!vwc@2o'
 
